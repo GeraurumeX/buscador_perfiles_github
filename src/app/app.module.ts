7b645d618cc7ea1f3f59fe29c6f65d3abb689dd1
gerardo agustin <gerardoagustin@hotmail.com>
2019-04-21 20:11:04 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./components/user/user.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { GithubService } from "./services/github.service";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, UserComponent, NotFoundComponent, HomeComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
